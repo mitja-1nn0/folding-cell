1e54c7dbc8b583d5f876468fce5a759c24997203
mitja-1nn0 <84024309+mitja-1nn0@users.noreply.github.com>
2021-09-03 16:03:56 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -31,14 +31,6 @@ let package = Package(
     platforms: [
         .iOS(.v9)
     ],
-    dependencies: [
-        // Here we define our package's external dependencies
-        // and from where they can be fetched:
-        .package(
-            url: "https://github.com/mitja-1nn0/folding-cell", 
-            .branch("master")
-        )
-    ],
     products: [
         .library(name: "FoldingCell",
             targets: ["FoldingCell"]),
